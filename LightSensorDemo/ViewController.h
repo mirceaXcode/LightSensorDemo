ed0440627d626825c9657c99d55e065722dd9d41
Mircea Popescu <mirceap@gmail.com>
2018-10-04 15:56:38 -0500

    Initial Commit

---
+++ LightSensorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightSensorDemo
+//
+//  Created by Mircea Popescu on 10/4/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
